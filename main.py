e7738f6ed7711d90a6136440ea3e7e90483849a2
emrecan312 <emrbayram63@gmail.com>
2025-06-06 01:15:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     response = ""
     if request.method == "POST":
         message = request.form.get("message")
         if message:
             response = "Merhaba Emre 💙"
-    return render_template("avatar.html", cevap=response)
+    return render_template("avatar.html", response=response)
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
